b394ce0152bedbf935739065305126dace649b31
SapiGithub <sapipantai1@gmail.com>
2023-08-23 09:13:41 +0700

    cleab

--- config/database.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const { Sequelize } = require('sequelize');
-
-module.exports =  new Sequelize(process.env.confDB, process.env.confUser, process.env.confPassword, {
-    host: process.env.confHost,
-    dialect: 'mariadb'
-  });
